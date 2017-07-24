b48b9671348b3856ff619aaabf9446e2000f58b7
CRAFTSMAN44 <30397564+CRAFTSMAN44@users.noreply.github.com>
2017-07-23 22:28:09 -0700

    stuff

---
+++ data/main.js
@@ -0,0 +1,23 @@
+var sketchProc=function(processingInstance){ with (processingInstance){
+
+button = function(con) {
+	x = con.x;
+	y = con.y;
+	width = con.width;
+	height = con.height;
+	text = con.text;
+};
+
+setup = function() {
+	size(800,600);
+	frameRate(60);
+	translate(0,0);
+	startbutton = button({x:width/2,y:height/2,width:150,height:80,text:"Start"});
+};
+
+draw = function() {
+	background(10,50,50);
+	
+};
+
+}};
